ab0d614260f853557311e646df97ce65d2e0fa75
Andrey Rogatin <AndreyR-Profit@yandex.ru>
2019-02-06 14:21:44 +0300

    update Model class

--- App/Models/Model.php
+++ App/Models/Model.php
@@ -17,4 +17,18 @@ abstract class Model
         $sql = 'SELECT * FROM ' . static::$table;
         return $db->query($sql, [], static::class);
     }
+
+    public static function findById($id)
+    {
+        $db = new Db;
+        $sql = 'SELECT * FROM ' . static::$table . ' WHERE id=:id';
+        $params = [':id' => $id];
+        $res = $db->query($sql, $params, static::class);
+
+        if (empty($res)) {
+            return false;
+        } else {
+            return $res[0];
+        }
+    }
 }
--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use App\Models\User;
 require __DIR__ . '/autoload.php';
 
 
-var_dump(Person::findAll());
+var_dump(Person::findById(8));
 
 $db = new Db;
 var_dump($db->query('SELECT * FROM persons'));
