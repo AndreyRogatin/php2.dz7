6b8e3e8adeede77083a6f62d7a5bd47bfe838a8d
Andrey Rogatin <AndreyR-Profit@yandex.ru>
2019-02-12 21:25:41 +0300

    implements Countable interface by View class

--- App/View.php
+++ App/View.php
@@ -3,7 +3,7 @@
 namespace App;
 
 
-class View
+class View implements \Countable
 {
     use Magic;
 
@@ -33,5 +33,19 @@ class View
     public function display($template)
     {
         echo $this->render($template);
+    }
+
+    /**
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     * @since 5.1.0
+     */
+    public function count()
+    {
+        return count($this->data);
     }
 }
