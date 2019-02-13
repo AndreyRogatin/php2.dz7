beb15d8dc602cb7e27f872a0640790d3702e7ade
Andrey Rogatin <AndreyR-Profit@yandex.ru>
2019-02-13 13:45:57 +0300

    implements Iterator interface by View class

--- App/View.php
+++ App/View.php
@@ -3,9 +3,17 @@
 namespace App;
 
 
-class View implements \Countable
+class View implements \Countable, \Iterator
 {
     use Magic;
+
+    protected $pos = 0;
+    protected $keys = [];
+
+    public function __construct()
+    {
+        $this->pos = 0;
+    }
 
     /**
      * @deprecated
@@ -47,5 +55,62 @@ class View implements \Countable
     public function count()
     {
         return count($this->data);
+    }
+
+    /**
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return $this->data[$this->keys[$this->pos]];
+    }
+
+    /**
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        ++$this->pos;
+    }
+
+    /**
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return $this->pos;
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return isset($this->keys[$this->pos]);
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        $this->pos = 0;
+        $this->keys = array_keys($this->data);
     }
 }
