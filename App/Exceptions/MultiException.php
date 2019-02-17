ecec836962eb3c8e29a1fd99df65878cfbaebc82
Andrey Rogatin <AndreyR-Profit@yandex.ru>
2019-02-17 17:59:47 +0300

    create MultiException class

---
+++ App/Exceptions/MultiException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Exceptions;
+
+
+class MultiException extends \Exception
+{
+    protected $errors = [];
+
+    public function add(\Exception $ex)
+    {
+        $this->errors[] = $ex;
+    }
+
+    public function all()
+    {
+        return $this->errors;
+    }
+
+    public function empty() : bool
+    {
+        return empty($this->errors);
+    }
+}
